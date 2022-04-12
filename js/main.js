b8d6feb7d9309654e79e09fa2462df5279fe954d
zhig <realbboyic@gmail.com>
2022-04-13 00:44:20 +0300

    13.04.2022 12:44AM

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 var tmp;
 var i = 0;
-setImg = function (el) {
+setImg = function (el, chill) {
 	if (el.getAttribute('flag') == '0'){
+		document.getElementById(chill).innerText = "257";
 		el.src = el.src.replace('src/likes.svg', 'src/full_like.png');
 		el.setAttribute('flag', 1);
 	}
 	else {
+		document.getElementById(chill).innerText = "256";
 		el.src = el.src.replace('src/full_like.png', 'src/likes.svg');
 		el.setAttribute('flag', 0);
 	}
